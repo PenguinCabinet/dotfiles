2f0de95ebe23dc5960a2ed723e9dedec7db3fad6
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-16 11:23:23 +0900

    Fix

--- copy_config.sh
+++ copy_config.sh
@@ -4,7 +4,7 @@ mkdir ~/Documents
 mkdir ~/Pictures
 
 echo "Link config files..."
-for e in .??*; do
+for e in ./*; do
     [ "$e" = ".git" ] && continue
     if [ "`echo $e | grep '.sh'`" ]; then
         continue
@@ -14,7 +14,7 @@ for e in .??*; do
 done
 
 cd .config
-for e in ??*; do
+for e in ./*; do
     [ "$e" = ".git" ] && continue
     if [ "`echo $e | grep '.sh'`" ]; then
         continue
