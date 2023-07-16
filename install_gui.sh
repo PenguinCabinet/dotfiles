8955968cdb609e71806d4047599a3080f56daaa1
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-16 12:05:58 +0900

    Fix

--- install_gui.sh
+++ install_gui.sh
@@ -1,4 +1,7 @@
 
+#Install xterm
+sudo apt install xterm -y
+
 #Install alacritty
 sudo apt install --no-install-recommends cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3 -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
