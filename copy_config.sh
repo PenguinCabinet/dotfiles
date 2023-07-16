af7e76b6bb736c6e7daf4a5765db0179487fee1f
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-16 11:15:51 +0900

    Fix

--- copy_config.sh
+++ copy_config.sh
@@ -6,7 +6,7 @@ mkdir ~/Pictures
 echo "Link config files..."
 for e in .??*; do
     [ "$e" = ".git" ] && continue
-    [ "$e" =~ ".sh" ] && continue
+    [ "`echo $e | grep '.sh'`" ] && continue
     rm -r "$HOME/$e"
     ln -s "$(pwd)/$e" "$HOME/$e"
 done
@@ -14,7 +14,7 @@ done
 cd config
 for e in ??*; do
     [ "$e" = ".git" ] && continue
-    [ "$e" =~ ".sh" ] && continue
+    [ "`echo $e | grep '.sh'`" ] && continue
     rm -r "$HOME/.config/$e"
     ln -s "$(pwd)/$e" "$HOME/.config/$e"
 done
