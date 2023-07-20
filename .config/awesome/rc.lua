5f080c4e5dd2cea1365bc28c01453e346da84f58
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-20 15:52:41 +0900

    Fix

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -276,8 +276,8 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r", awesome.restart,
-              {description = "reload awesome", group = "awesome"}),
+    --awful.key({ modkey, "Control" }, "r", awesome.restart,
+    --          {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
