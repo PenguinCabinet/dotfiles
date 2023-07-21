b43573d261374b5a8c64247b07fedc6b88928005
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-21 10:08:49 +0900

    Update

--- install_ws.sh
+++ install_ws.sh
@@ -1,5 +1,6 @@
 
 echo "Install awesome(window system)..."
 sudo apt install --no-install-recommends lightdm awesome xinit xorg -y
+sudo apt install --no-install-recommends fonts-noto-cjk fonts-noto-cjk-extra ttf-ancient-fonts-symbola fonts-noto-color-emoji -y
 sudo dpkg-reconfigure lightdm 
 sudo apt install rofi -y
