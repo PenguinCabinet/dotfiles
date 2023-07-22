61066d4ecc472afea1a3b891aab9a2f97a6910ca
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-22 20:51:35 +0900

    Add commentcomment

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 apt update -y
 apt upgrade -y
 
+#Don't ask me
 sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sed --in-place -e '/auth.*required.*pam_shells.so/s/required/sufficient/g' /etc/pam.d/chsh
 
