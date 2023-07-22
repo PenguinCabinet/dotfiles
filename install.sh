7207a892dc38c76a3b512c5f7d75c3cfb44557c2
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-22 20:42:52 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 apt update -y
 apt upgrade -y
+
 sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
+sed --in-place -e '/auth.*required.*pam_shells.so/s/required/sufficient/g' /etc/pam.d/chsh
 
 bash ./install_cui.sh
 bash ./install_ws.sh
