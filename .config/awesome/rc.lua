1b949854680a5c970985520efa7bbde5c7a4b294
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-23 21:19:43 +0900

    Change rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -598,7 +598,7 @@ client.connect_signal("request::titlebars", function(c)
         },
         { -- Right
             awful.titlebar.widget.floatingbutton (c),
-            awful.titlebar.widget.maximizedbutton(c),
+            --awful.titlebar.widget.maximizedbutton(c),
             --awful.titlebar.widget.stickybutton   (c),
             --awful.titlebar.widget.ontopbutton    (c),
             awful.titlebar.widget.closebutton    (c),
