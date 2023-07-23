cf64d53db260c0a6f05f1da574ba5f603ce005b6
PenguinCabinet <kage2.kage22@gmail.com>
2023-07-23 21:36:34 +0900

    Fix themes_path

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -1,2 +1,3 @@
+local themes_path = gfs.get_themes_dir()
 theme.titlebar_close_button_normal = themes_path.."default/icons/close_normal.png"
 theme.titlebar_close_button_focus  = themes_path.."default/icons/close_focus.png"
