439193457d176122f9503519ffe5717b501a79c3
PenguinCabinet <contact@penguincabinet.com>
2023-07-28 21:30:31 +0900

    Add endable deb-src

--- init_config.sh
+++ init_config.sh
@@ -1,9 +1,12 @@
 timedatectl set-timezone Asia/Tokyo
 
 #Don't ask me
-sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 sed --in-place -e '/auth.*required.*pam_shells.so/s/required/sufficient/g' /etc/pam.d/chsh
 
 # git config
 git config --global user.name PenguinCabinet
 git config --global user.email contact@penguincabinet.com
+
+# endable deb-src 
+sed -i 's/# deb-src /deb-src /g' /etc/apt/sources.list
