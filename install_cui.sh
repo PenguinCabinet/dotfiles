552d138112830dc6bdf5c66ea256713105d84fc1
PenguinCabinet <contact@penguincabinet.com>
2023-07-28 21:08:39 +0900

    Install Github cli

--- install_cui.sh
+++ install_cui.sh
@@ -2,6 +2,9 @@
 echo "Install basic softwears..."
 apt install --no-install-recommends wget gpg fzf -y
 
+echo "Install Github cli"
+apt install --no-install-recommends gh -y
+
 echo "Install fish..."
 apt-add-repository ppa:fish-shell/release-3 -y
 apt-get update -y
