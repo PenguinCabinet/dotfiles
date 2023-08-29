ff7900a6757fe3f303576cdeeb8cb278d3c36b20
PenguinCabinet <kage2.kage22@gmail.com>
2023-08-29 20:19:49 +0900

    Change i3vm

--- install_ws.sh
+++ install_ws.sh
@@ -2,9 +2,7 @@
 echo "Install sway(window system)..."
 apt install --no-install-recommends lightdm -y
 
-apt install --no-install-recommends sway -y
-apt install --no-install-recommends sway-backgrounds -y
-apt install --no-install-recommends foot -y
+apt install --no-install-recommends i3 xinit xorg -y
 
 apt install --no-install-recommends fonts-ubuntu fonts-noto-cjk fonts-noto-cjk-extra ttf-ancient-fonts-symbola fonts-noto-color-emoji -y
 apt install --no-install-recommends language-pack-ja -y
