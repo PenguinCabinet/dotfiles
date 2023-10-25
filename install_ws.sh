14c94687494424ee85550f60402ade54e1f545c3
PenguinCabinet <kage2.kage22@gmail.com>
2023-10-25 17:45:06 +0900

    Add i3blocks

--- install_ws.sh
+++ install_ws.sh
@@ -3,6 +3,7 @@ echo "Install sway(window system)..."
 apt install --no-install-recommends lightdm -y
 
 apt install --no-install-recommends i3 xinit xorg -y
+apt install -y i3blocks
 
 apt install --no-install-recommends fonts-ubuntu fonts-noto-cjk fonts-noto-cjk-extra ttf-ancient-fonts-symbola fonts-noto-color-emoji -y
 apt install --no-install-recommends language-pack-ja -y
