e04580266df56ea55a3089c3e84d56717f843999
PenguinCabinet <kage2.kage22@gmail.com>
2026-04-18 16:02:14 +0900

    .wezterm.luaの修正

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,3 +1,9 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
 config.use_ime = true
 return config
 
