6bc59f07a213238a5c0ec17e3d3d4c6af700b240
PenguinCabinet <kage2.kage22@gmail.com>
2026-04-19 10:57:18 +0900

    weztermをF11で全画面化する

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,6 +4,13 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.keys = {
+  {
+    key = 'F11',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
 config.use_ime = true
 return config
 
