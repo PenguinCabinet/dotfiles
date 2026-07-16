0e1be236e02b66a22d82e00ad32faadfde840aec
PenguinCabinet <kage2.kage22@gmail.com>
2026-07-16 23:47:44 +0900

    install.shを様々な環境で実行できるよう改善

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-stow -v vim wezterm bash zsh tmux i3 "Do-not-use-PC"
+for package in vim wezterm bash zsh tmux i3 "Do-not-use-PC"; do
+    stow \
+      --verbose=2 \
+      --target="$HOME" \
+      "$package" || printf 'Failed: %s\n' "$package" >&2
+done
+
+
+#stow -v vim wezterm bash zsh tmux i3 "Do-not-use-PC"
